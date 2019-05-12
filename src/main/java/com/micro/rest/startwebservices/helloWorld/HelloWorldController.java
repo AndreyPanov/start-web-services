dcfc68290c54a2bd515e624640d12602207c2a46
Andrey Panov <andrey.panov@blacklane.com>
2019-05-12 20:38:46 +0200

    Add dependencies

--- src/main/java/com/micro/rest/startwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/com/micro/rest/startwebservices/helloWorld/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.micro.rest.startwebservices.helloWorld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
 	//GET
 	//URI - /hello-world
 	//method
@@ -27,5 +30,11 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean/path-variable/{name}")
 	public HelloWorldBean helloWorldBean(@PathVariable String name) {
 		return new HelloWorldBean("Hello World " + name);
+
+	}
+
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
