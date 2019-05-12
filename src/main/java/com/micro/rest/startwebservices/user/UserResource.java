22826516747fc844819f5879afc8223338777959
Andrey Panov <andrey.panov@blacklane.com>
2019-05-12 12:38:04 +0200

    Update IDE

--- src/main/java/com/micro/rest/startwebservices/user/UserResource.java
+++ src/main/java/com/micro/rest/startwebservices/user/UserResource.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 @RestController
 public class UserResource {
@@ -29,12 +33,16 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
-		if(user==null) {
+		if(user==null)
 			throw new UserNotFoundException("id - " + id);
-		}
+		
+		//HATEOAS
+		//Resource<User> resource = new Resource<User>(user);
+		//ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		//resource.add(linkTo.withRel("all-users"));
 		return user;
 	}
-	
+
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 		User user = service.deleteById(id);
